5bbb5d67b3296dc7d7f8b570ca292129b3c48978
Nick Steelman <steelman@students.olin.edu>
2016-11-29 17:04:07 -0500

    fuk yo commit

--- baseballSweep.m
+++ baseballSweep.m
@@ -1,20 +1,27 @@
+clf
+clear
 a = 0;
 b = 0;
 for i = 1.007:.01:1.225
     b = b + 1;
+    a = 0;
+    
     for j = 32.6:.1:36.6
         
         a= a + 1;
         res = baseballPitch(i, j)
-        if (res(1) < .56 || res(1) < 1.56 )
-            strike(b,a) = 1;
-        else
+       
+        speed(a) =j;
+       
             strike(b,a) = abs(.56-res(1));
-        end
+            
+            density(b) = i;
+       
+        %}
     end
     
 end
-        
-
  
- pcolor(strike)
+
+ pcolor(speed,density,  strike,[.56]);
+
