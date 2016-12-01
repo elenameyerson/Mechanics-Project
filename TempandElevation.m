c7a41b44d82d84084a99775efd230807b1bb3049
Nick Steelman <steelman@students.olin.edu>
2016-12-01 16:55:06 -0500

    ugh

---
+++ TempandElevation.m
@@ -0,0 +1,3 @@
+clf;
+clear;
+velocity = 5.14 * density + 28.3;
--- baseballSweep.m
+++ baseballSweep.m
@@ -2,15 +2,16 @@ clf
 clear
 a = 0;
 b = 0;
-for i = 1.007:.01:1.225
+for i = .47:.005:1.825
     b = b + 1;
     a = 0;
     
-    for j = 32.6:.1:36.6
+    for j = 29.6:.1:40.6
         
         a= a + 1;
-        res = baseballPitch(i, j)
+        res = baseballPitch(i, j);
        
+        %{
         speed(a) =j;
        
             strike(b,a) = abs(.56-res(1));
@@ -18,10 +19,30 @@ for i = 1.007:.01:1.225
             density(b) = i;
        
         %}
+        
+        diff(a) = abs(.56-res(1));
+        speed(a) = j;
     end
+    [~,speedminI(b)] = min(diff);
+    speedmin(b) = speed(speedminI(b));
     
+    density(b) = i;
 end
- 
 
- pcolor(speed,density,  strike,[.56]);
+plot(density,speedmin);
+coeffs = polyfit(density, speedmin, 1)
+% Get fitted values
+fittedX = linspace(min(density), max(density), 200);
+fittedY = polyval(coeffs, fittedX);
+% Plot the fitted line
+hold on;
+plot(fittedX, fittedY, 'r-', 'LineWidth', 3);
 
+%{
+grid off
+ pcolor(density,speed,  strike.');
+xlabel('Air Density (kg/m^3)')
+ylabel('Baseball Speed (m/s)')
+c = colorbar
+c.Label.String = 'Distance from bottom of strike zone (m)'
+%}
