6f3aa57c75b8e8377e34309792f508e4bda811aa
George Steelman <gsteelman@olin.edu>
2016-12-04 13:25:22 -0500

    sweep through temp and elevation

--- TempandElevation.m
+++ TempandElevation.m
@@ -1,3 +1,16 @@
 clf;
 clear;
 velocity = 5.14 * density + 28.3;
+a = 0;
+b = 0;
+for i = 0 : tp : 6000
+    a = a+1;
+    b =0;
+    elevation(a) = i;
+    for j = 0: tp : 30
+        temp(b) = j;
+        
+        
+        
+    end
+end
